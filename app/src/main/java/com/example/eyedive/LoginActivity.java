79aac92152bf6d34ee76c007c1c2e410f8a3c060
Im-itachi <akashkarunakar@gmail.com>
2020-11-27 13:42:24 +0530

    till login

--- app/src/main/java/com/example/eyedive/LoginActivity.java
+++ app/src/main/java/com/example/eyedive/LoginActivity.java
@@ -1,14 +1,98 @@
 package com.example.eyedive;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.eyedive.Model.Users;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText InputPhoneNumber,InputPassword;
+    private Button LoginButton;
+    private ProgressDialog LoadingBar;
+    private String dbname = "Users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        LoginButton = (Button) findViewById(R.id.login_btn);
+        InputPhoneNumber = (EditText) findViewById(R.id.login_phone_number_input);
+        InputPassword = (EditText) findViewById(R.id.login_password_input);
+        LoadingBar= new ProgressDialog(this);
+
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginUser();
+            }
+        });
+    }
+
+    private void LoginUser() {
+        String phone = InputPhoneNumber.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        if(TextUtils.isEmpty(phone)){
+            Toast.makeText(this, "Enter correct phone number", Toast.LENGTH_SHORT).show();
+        }else if(TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "password must contains upper case,lower case alphabet,digits and special characters.", Toast.LENGTH_SHORT).show();
+        }else{
+            LoadingBar.setTitle("Login Account");
+            LoadingBar.setMessage("please wait");
+            LoadingBar.setCanceledOnTouchOutside(false);
+            LoadingBar.show();
+
+            AllowAccessToAccount(phone,password);
+        }
+
+    }
+
+    private void AllowAccessToAccount(String phone, String password) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child(dbname).child(phone).exists()){
+                    Users usersdata = snapshot.child(dbname).child(phone).getValue(Users.class);
+                     if (usersdata.getPhone().equals(phone)){
+                         if (usersdata.getPassword().equals(password)){
+                             Toast.makeText(LoginActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                             LoadingBar.dismiss();
+                             Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                             startActivity(intent);
+                         }else {
+                             Toast.makeText(LoginActivity.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
+                         }
+                     }
+                }else {
+                    Toast.makeText(LoginActivity.this, "Account with this" + phone + "DOESN'T exists", Toast.LENGTH_SHORT).show();
+                    LoadingBar.dismiss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/eyedive/Prevalent/Prevalent.java
@@ -0,0 +1,7 @@
+package com.example.eyedive.Prevalent;
+
+import com.example.eyedive.Model.Users;
+
+public class Prevalent {
+    private static Users CurrentonlineUser;
+}
