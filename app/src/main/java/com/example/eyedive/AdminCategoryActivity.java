9e66d3601773acaea14b4060e7e7fcbdc64c506c
im-itachi <akashkarunakar@gmail.com>
2020-12-08 15:35:16 +0530

    category

--- app/src/main/java/com/example/eyedive/AdminCategoryActivity.java
+++ app/src/main/java/com/example/eyedive/AdminCategoryActivity.java
@@ -2,13 +2,66 @@ package com.example.eyedive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AdminCategoryActivity extends AppCompatActivity {
+    private ImageView glasses, hatsCaps, walletsBagsPurses, shoes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_category);
+
+        glasses = (ImageView) findViewById(R.id.glasses);
+        hatsCaps = (ImageView) findViewById(R.id.hats_caps);
+        walletsBagsPurses = (ImageView) findViewById(R.id.purses_bags_wallets);
+        shoes = (ImageView) findViewById(R.id.shoes);
+
+        glasses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Glasses");
+                startActivity(intent);
+            }
+        });
+
+
+        hatsCaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Hats Caps");
+                startActivity(intent);
+            }
+        });
+
+
+
+        walletsBagsPurses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Wallets Bags Purses");
+                startActivity(intent);
+            }
+        });
+
+
+        shoes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Shoes");
+                startActivity(intent);
+            }
+        });
     }
 }
