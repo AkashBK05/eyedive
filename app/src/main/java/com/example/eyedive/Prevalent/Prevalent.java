86fe45454dd19263ace880d73926afe4213e0176
im-itachi <akashkarunakar@gmail.com>
2020-12-28 11:28:13 +0530

    cart user

--- app/src/main/java/com/example/eyedive/Prevalent/Prevalent.java
+++
@@ -1,9 +0,0 @@
-package com.example.eyedive.Prevalent;
-
-import com.example.eyedive.Model.Users;
-
-public class Prevalent {
-    private static Users CurrentonlineUser;
-    public static final String UserPhoneKey = "9998877445";
-    public static final String UserPasswordkey = "password";
-}
